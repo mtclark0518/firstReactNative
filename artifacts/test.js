d54f1fa8d0260d030b6155479e1402bbb1a21f41
Tyler Clark <mtclark0518@gmail.com>
2017-09-11 17:04:23 -0600

    adds typescript

---
+++ artifacts/test.js
@@ -0,0 +1,6 @@
+export class helloWorld {
+    constructor() {
+        this.statement = 'hello World';
+    }
+}
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+
+export  class helloWorld{
+statement:  string = 'hello World'
+}
+
